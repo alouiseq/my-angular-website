06ebd535f2a4069389d221e4899e8abbcfac15dd
Alouise Quiatchon <alouiseq@gmail.com>
2015-07-12 01:53:51 -0700

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mywebsiteApp
+ * @description
+ * # mywebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mywebsiteApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'ProjectsCtrl'
+      })
+      .when('/resume', {
+        templateUrl: 'views/resume.html',
+        controller: 'ResumeCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/resume.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mywebsiteApp.controller:MainCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the mywebsiteApp
+ */
+angular.module('mywebsiteApp')
+    .controller('ResumeCtrl', function ($scope) {
+                       
+    });
